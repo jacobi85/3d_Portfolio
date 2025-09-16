f88a7d73183f2846ed0037308c9d33ce92d7d02f
jacobi85 <hello@jacobwestman.com>
2025-09-16 15:34:01 +0200

    add tailwind + assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <main>
       <div>
-        <h1>Welcome to my 3d Portfolio</h1>
+        <h1 className="text-3xl">Welcome to my 3d Portfolio</h1>
       </div>
     </main>
   );
