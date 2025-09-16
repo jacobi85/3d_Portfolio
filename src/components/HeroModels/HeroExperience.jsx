fbd30acc9d176e8003f6f3d0c820f0a07b0a60c6
jacobi85 <hello@jacobwestman.com>
2025-09-16 17:50:37 +0200

    fix mindistance of panning the room

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -5,18 +5,17 @@ import { Room } from "./Room";
 import HeroLights from "./HeroLights";
 
 const HeroExperience = () => {
-  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <OrbitControls
-        enablePan={false}
-        enableZoom={!isTablet}
-        maxDistance={20}
-        minDistance={20}
-        minPolarAngle={Math.PI / 5}
-        maxPolarAngle={Math.PI / 2}
+        enablePan={false} // Prevents panning of the scene
+        enableZoom={!isTablet} // Disables zoom on tablets
+        maxDistance={20} // Maximum distance for zooming out
+        minDistance={5} // Minimum distance for zooming in
+        minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
+        maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
       />
 
       <HeroLights />
