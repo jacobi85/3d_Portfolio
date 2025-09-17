f5b0e354d6a000a1a81aeca7d98d3c18231d9e8f
jacobi85 <hello@jacobwestman.com>
2025-09-17 11:46:38 +0200

    add logoSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoSection from "./sections/LogoSection";
+import FeatureCards from "./sections/FeatureCards";
+
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
     </>
   );
 };
